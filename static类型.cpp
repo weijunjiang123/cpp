bc98b0496378a17196a54634e02512637c6da408
weijunjiang123 <1366202829@qq.com>
2023-10-22 22:55:18 +0800

    change file

--- static类型.cpp
+++ static类型.cpp
@@ -2,7 +2,7 @@
 
 void func(void); 
 
-static int count = 10; /* 全局变量 */
+static int count = 10; // 全局变量: static 修饰, 只能在本文件中使用, 不能被其他文件访问, 也不能被其他文件修改, 但是可以被其他文件定义同名变量覆盖
 
  int main(){
     while(count--)
@@ -12,7 +12,7 @@ static int count = 10; /* 全局变量 */
     return 0;
 }
 void func( void ){
-    static int i = 5; // 局部静态变量
+    static int i = 5; // 局部静态变量: static 修饰, 只能在本函数中使用, 不能被其他函数访问, 也不能被其他函数修改, 但是可以被其他函数定义同名变量覆盖
     i++;
     std::cout << "变量 i 为 " << i ;
     std::cout << " , 变量 count 为 " << count << std::endl;
--- 重载.cpp
+++ 重载.cpp
@@ -1,48 +1,65 @@
 #include <iostream>
 using namespace std;
 
-class Box{
-    public:
-        void setLength(double len){
-            length = len;
-        }
-        void setWeight(double wei){
-            weight = wei;
-        }
-        void setHeight(double hei){
-            height = hei;
-        }
-        void setBox(double len, double wei, double hei){
-            length = len;
-            weight = wei;
-            height = hei;
-        }
-        void printBox(){
-            cout << "Length: " << length << endl;
-            cout << "Weigth: " << weight << endl;
-            cout << "Height: " << height << endl;
-        }
-        Box operator+(const Box& b){
-            Box box;
-            box.length = this->length + b.length;
-            box.weight = this->weight + b.weight;
-            box.height = this->height + b.height;
-            return box;
-        }
+class Box
+{
+public:
+    // 构造函数
+    Box(double len, double wei, double hei) : length(len), weight(wei), height(hei){};
+    // 拷贝构造函数
+    Box(const Box &b)
+    {
+        length = b.length;
+        weight = b.weight;
+        height = b.height;
+    }
+    // 默认构造函数
+    Box():length(0), weight(0), height(0){};
 
-    protected:
-        double length;
-        double weight;
-        double height;
+    // 析构函数
+    ~Box(){};
+
+    // 对外的接口
+    void setLength(double len)
+    {
+        length = len;
+    }
+    void setWeight(double wei)
+    {
+        weight = wei;
+    }
+    void setHeight(double hei)
+    {
+        height = hei;
+    }
+    void printBox()
+    {
+        cout << "Length: " << length << "\t";
+        cout << "Weigth: " << weight << "\t";
+        cout << "Height: " << height << endl;
+    }
+
+    // 重载运算符，实现Box类对象的加法运算
+    Box operator+(const Box &b)
+    {
+        Box box;
+        box.length = this->length + b.length;
+        box.weight = this->weight + b.weight;
+        box.height = this->height + b.height;
+        return box;
+    }
+
+protected:
+    double length;
+    double weight;
+    double height;
 };
 
-
-int main(){
-    Box box1;
-    Box box2;
+int main()
+{
+    Box box1(1.0, 2.0, 5.5);
+    Box box2(3.0, 4.8, 3.1);
     Box box3;
-    box1.setBox(1.0, 2.0, 5.5);
-    box2.setBox(3.0, 4.8, 3.1);
     box3 = box1 + box2;
     cout << "box1: " << endl;
     box1.printBox();
@@ -50,7 +67,4 @@ int main(){
     box2.printBox();
     cout << "box3: " << endl;
     box3.printBox();
-
-
 }
-
