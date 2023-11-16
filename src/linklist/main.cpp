f0ab668a828255a9e3bcef1d736970b3710bf6c8
weijunjiang123 <1366202829@qq.com>
2023-11-16 21:57:25 +0800

    add linklist and socket

---
+++ src/linklist/main.cpp
@@ -0,0 +1,75 @@
+#include "linkList.h"
+#include <iostream>
+
+void test()
+{
+    List<int> list;
+    list.insertAsFirst(1);
+    list.insertAsFirst(2);
+    list.insertAsFirst(3);
+    list.insertAsFirst(4);
+    list.insertAsFirst(5);
+    list.insertAsFirst(6);
+    list.insertAsFirst(7);
+    list.insertAsFirst(8);
+    list.insertAsFirst(9);
+    list.insertAsFirst(10);
+    list.insertAsFirst(11);
+    list.insertAsFirst(12);
+    list.insertAsFirst(13);
+    list.insertAsFirst(14);
+    list.insertAsFirst(15);
+    list.insertAsFirst(16);
+    list.insertAsFirst(17);
+    list.insertAsFirst(18);
+    list.insertAsFirst(19);
+    list.insertAsFirst(20);
+    list.insertAsFirst(21);
+    list.insertAsFirst(22);
+    list.insertAsFirst(23);
+    list.insertAsFirst(24);
+    list.insertAsFirst(25);
+    list.insertAsFirst(26);
+    list.insertAsFirst(27);
+    list.insertAsFirst(28);
+    list.insertAsFirst(29);
+    list.insertAsFirst(30);
+    list.insertAsFirst(31);
+    list.insertAsFirst(32);
+    list.insertAsFirst(33);
+    list.insertAsFirst(34);
+    list.insertAsFirst(35);
+    list.insertAsFirst(36);
+    list.insertAsFirst(37);
+    list.insertAsFirst(38);
+    list.insertAsFirst(39);
+    list.insertAsFirst(40);
+    list.insertAsFirst(41);
+    list.insertAsFirst(42);
+    list.insertAsFirst(43);
+    list.insertAsFirst(44);
+    list.insertAsFirst(45);
+    list.insertAsFirst(46);
+    list.insertAsFirst(47);
+    list.insertAsFirst(48);
+    list.insertAsFirst(49);
+    list.insertAsFirst(50);
+    list.insertAsFirst(51);
+    list.insertAsFirst(52);
+    list.insertAsFirst(53);
+    list.insertAsFirst(54);
+    list.insertAsFirst(55);
+    list.insertAsFirst(56);
+    list.insertAsFirst(57);
+    list.insertAsFirst(58);
+    list.insertAsFirst(59);
+    list.insertAsFirst(60);
+    list.insertAsFirst(61);
+    list.printList();
+    list.remove(list, 1);
+}
+
+int main()
+{
+    test();
+}
